6a24f9fac50bec9f2ff3f29d3c852bf2477a0aa2
Richkum <kumatum2@gmail.com>
2023-10-04 13:59:56 +0100

    testing

--- op.js
+++ op.js
@@ -1,3 +1,49 @@
-// 
+// const nora = {
+//   name: "Nora",
+//   email: "n@gmail.com",
+//   batch: "October 23",
+//   print() {
+//     console.log(`${this.name} ${this.email} is from ${this.batch} batch`)
+//   }
+// }
+
+// class Student {
+//   constructor(name, email, batch) {
+//     this.name = name;
+//     this.email = email;
+//     this. batch = batch; 
+//   }
+
+//   print () {
+//     console.log(`${this.formattedLablel} is from ${this.batch} batch`)
+//   }
+//    get formattedLablel () {
+//     return `${this.name}(${this.email})`
+//    }
+
+//    set formattedLablel(value) {
+//     const [name, batch] = value.split("(");
+//     console.log(name, batch);
+//     this.name = name;
+//     this.email = this.email.split(")").shift();
+//    }
+// }
+
+// // function Student(name, email, batch) {
+// //   this.name = name;
+// //   this.email = email;
+// //   this. batch = batch;
+// //   print = () => {
+    
+// //   }
+// //   return this;
+// // }
+
+// const dorine = new Student("Dorine", "d@gmail.com", "JUly 23");
+
+// dorine.print();
+// nora.print();
+
+
 
 
